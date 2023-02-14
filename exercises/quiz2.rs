b162bbaeb46700d79eef91f29093252f0514a0b4
Xin Zhang <zhangxin.xa@gmail.com>
2023-02-14 17:03:45 +0800

    work 4

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -49,7 +49,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use my_module::transformer;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
